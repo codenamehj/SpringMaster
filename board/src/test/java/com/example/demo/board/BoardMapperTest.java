418f90b1c1261982bf4126eca14a574c5df29aad
codenamehj <loveim1212@naver.com>
2024-03-13 23:39:38 +0900

    20240313 집

--- board/src/test/java/com/example/demo/board/BoardMapperTest.java
+++ board/src/test/java/com/example/demo/board/BoardMapperTest.java
@@ -17,18 +17,18 @@ public class BoardMapperTest {
 	public void 게시판전체조회() {
 		BoardVO vo = new BoardVO();
 		vo.setEnd(50);
-//		vo.setWriter("홍");
+		vo.setWriter("홍");
 //		vo.setTitle("안녕");
 		List<BoardVO> list = mapper.getBoardList(vo);
 		for (BoardVO boardVO : list) {
-			System.out.println(boardVO.getBoardNo() + boardVO.getTitle() + boardVO.getWriter());
+			System.out.println(boardVO.getBoardNo() + " " + boardVO.getTitle() + " " + boardVO.getWriter());
 			;
 		}
 	}
 
 	@Test
 	public void 게시판단건조회() {
-		int boardNo = 61;
+		int boardNo = 41;
 		BoardVO vo = mapper.getBoardInfo(boardNo);
 		System.out.println(vo);
 	}
@@ -41,10 +41,18 @@ public class BoardMapperTest {
 		System.out.println("등록된 게시판 번호 : " + vo.getBoardNo());
 		System.out.println("등록된 게시글 수 : " + result);
 	}
-	
+
 	@Test
 	public void 게시글삭제() {
-		int result = mapper.deleteBoard(61);
+		int result = mapper.deleteBoard(43);
 		System.out.println("삭제건수 = " + result);
+	}
+	
+	@Test
+	public void 게시글수정() {
+		BoardVO vo = BoardVO.builder().boardNo(42).title("집에 갈래").content("가즈아~~!!").build();
+		int result = mapper.updateBoard(vo);
+		
+		System.out.println("수정된 게시글 수 : " + result);
 	}
 }
